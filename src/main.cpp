782c2dbe4a456dfa4e15b0fd20472278dc6bb009
banimania <dani17val+1@gmail.com>
2024-03-09 15:27:49 +0100

    Win conditions

--- src/main.cpp
+++ src/main.cpp
@@ -31,7 +31,7 @@ void placeBlock(int* board, int n, int x, int z, int p) {
   *(board + x * n * n + highest * n + z) = p;
 }
 
-void renderBoard(int* board, int n, Vector2 sel, float s) {
+void renderBoard(int* board, int n, Vector2 sel, float s, int g) {
   //Render Grid
   Color gridColor = Color{255, 255, 255, 255};
   for (float i = 0; i < n; i++) {
@@ -57,9 +57,142 @@ void renderBoard(int* board, int n, Vector2 sel, float s) {
     }
   }
 
-  Vector3 pos = {sel.x * s + s / 2.0f, s / 2.0f + getNextHighestBlock(board, n, sel.x, sel.y) * s, sel.y * s + s / 2.0f};
-  DrawCube(pos, s, s, s, {255, 255, 255, 55});
-  DrawCubeWires(pos, s, s, s, {255, 255, 255, 255});
+  if (!g) {
+    Vector3 pos = {sel.x * s + s / 2.0f, s / 2.0f + getNextHighestBlock(board, n, sel.x, sel.y) * s, sel.y * s + s / 2.0f};
+    DrawCube(pos, s, s, s, {255, 255, 255, 55});
+    DrawCubeWires(pos, s, s, s, {255, 255, 255, 255});
+  }
+}
+
+bool checkWinner(int* board, int n, int p, int x) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      for (int k = 0; k < n; k++) {
+        if (*(board + i * n * n + j * n + k) == p) {
+          int c = 0;
+          
+          //Horizontal
+          do {
+            if (*(board + (c + i) * n * n + j * n + k) == p) c++;
+            else c = 0;
+
+            if (c == x) return 1;
+          } while (c);
+          
+          //Vertical
+          do {
+            if (*(board + i * n * n + j * n + k + c) == p) c++;
+            else c = 0;
+
+            if (c == x) return 1;
+          } while (c);
+          
+          //Diagonal ascending plane
+          do {
+            if (*(board + (i - c) * n * n + j * n + k + c) == p) c++;
+            else c = 0;
+
+            if (c == x) return 1;
+          } while (c);
+
+
+          //Diagonal descending plane
+          do {
+            if (*(board + (i + c) * n * n + j * n + k + c) == p) c++;
+            else c = 0;
+
+            if (c == x) return 1;
+          } while (c);
+
+          //Up
+          do {
+            if (*(board + i * n * n + (j + c) * n + k) == p) c++;
+            else c = 0;
+
+            if (c == x) return 1;
+          } while (c);
+          
+
+          //3D diag 1
+          do {
+            if (*(board + (i + c) * n * n + (j + c) * n + k) == p) c++;
+            else c = 0;
+
+            if (c == x) return 1;
+          } while (c);
+
+
+          //3D diag 2
+          do {
+            if (*(board + (i - c) * n * n + (j + c) * n + k) == p) c++;
+            else c = 0;
+
+            if (c == x) return 1;
+          } while (c);
+
+
+          //3D diag 3
+          do {
+            if (*(board + i * n * n + (j + c) * n + (k + c)) == p) c++;
+            else c = 0;
+
+            if (c == x) return 1;
+          } while (c);
+
+
+          //3D diag 4
+          do {
+            if (*(board + i * n * n + (j + c) * n + (k - c)) == p) c++;
+            else c = 0;
+
+            if (c == x) return 1;
+          } while (c);
+
+          
+          //3D diag 5
+          do {
+            if (*(board + (i - c) * n * n + (j + c) * n + (k - c)) == p) c++;
+            else c = 0;
+
+            if (c == x) return 1;
+          } while (c);
+
+
+          //3D diag 6
+          do {
+            if (*(board + (i + c) * n * n + (j + c) * n + (k - c)) == p) c++;
+            else c = 0;
+
+            if (c == x) return 1;
+          } while (c);
+
+          //3D diag 7
+          do {
+            if (*(board + (i - c) * n * n + (j + c) * n + (k + c)) == p) c++;
+            else c = 0;
+
+            if (c == x) return 1;
+          } while (c);
+
+
+          //3D diag 8
+          do {
+            if (*(board + (i + c) * n * n + (j + c) * n + (k + c)) == p) c++;
+            else c = 0;
+
+            if (c == x) return 1;
+          } while (c);
+        }
+      }
+    }
+  }
+  return 0;
+}
+
+int getBoardState(int* board, int n, int x) {
+  if (checkWinner(board, n, 1, x)) return 1;
+  else if (checkWinner(board, n, 2, x)) return 2;
+  return 0;
 }
 
 int main() {
@@ -69,9 +202,15 @@ int main() {
 
   float s = 5.0f;
 
+  int g = 0;
+
   int n;
   std::cout << "Board size: ";
   std::cin >> n;
+
+  int x;
+  std::cout << "Number of connections in order to win: ";
+  std::cin >> x;
 
   int* board = new int[n * n * n];
   resetBoard(board, n);
@@ -93,21 +232,23 @@ int main() {
   int p = 1;
   while(!WindowShouldClose()) {
     //Handle Input
-    if (IsKeyPressed(KEY_W)) {
-      sel.x--;
-      if (sel.x < 0) sel.x = 0;
-    } else if (IsKeyPressed(KEY_S)) {
-      sel.x++;
-      if (sel.x >= n) sel.x = n - 1;
-    } else if (IsKeyPressed(KEY_A)) {
-      sel.y++;
-      if (sel.y >= n) sel.y = n - 1;
-    } else if (IsKeyPressed(KEY_D)) {
-      sel.y--;
-      if (sel.y < 0) sel.y = 0;
-    } else if (IsKeyPressed(KEY_SPACE)) {
-      placeBlock(board, n, sel.x, sel.y, p);
-      p = 3 - p;
+    if (!g) {
+      if (IsKeyPressed(KEY_W)) {
+        sel.x--;
+        if (sel.x < 0) sel.x = 0;
+      } else if (IsKeyPressed(KEY_S)) {
+        sel.x++;
+        if (sel.x >= n) sel.x = n - 1;
+      } else if (IsKeyPressed(KEY_A)) {
+        sel.y++;
+        if (sel.y >= n) sel.y = n - 1;
+      } else if (IsKeyPressed(KEY_D)) {
+        sel.y--;
+        if (sel.y < 0) sel.y = 0;
+      } else if (IsKeyPressed(KEY_SPACE)) {
+        placeBlock(board, n, sel.x, sel.y, p);
+        p = 3 - p;
+      }
     }
 
     //UpdateCamera(camera, CAMERA_PERSPECTIVE);
@@ -118,14 +259,19 @@ int main() {
 
     BeginMode3D(*camera);
 
-    renderBoard(board, n, sel, s);
+    renderBoard(board, n, sel, s, g);
 
     EndMode3D();
-
-    std::string moveString = "Player " + std::to_string(p) + " moves";
-    DrawText(moveString.c_str(), SCREEN_WIDTH / 2.0f - MeasureText(moveString.c_str(), 30.0f) / 2.0f, SCREEN_HEIGHT - 50.0f, 30.0f, {255, 255, 255, 255});
     
+    std::string moveString = "Player " + std::to_string(p) + " moves";
+    if (g) {
+      moveString = "Player " + std::to_string(g) + " wins!";
+    }
+    DrawText(moveString.c_str(), SCREEN_WIDTH / 2.0f - MeasureText(moveString.c_str(), 30.0f) / 2.0f, SCREEN_HEIGHT - 50.0f, 30.0f, {255, 255, 255, 255});
+
     EndDrawing();
+    
+    g = getBoardState(board, n, x);
   }
 
   CloseWindow();

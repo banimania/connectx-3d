3f741b5d9d2d63abc1aff705dd9f2cd0e93a012b
banimania <dani17val+1@gmail.com>
2024-03-09 13:53:19 +0100

    Place

--- src/main.cpp
+++ src/main.cpp
@@ -5,29 +5,41 @@
 
 void resetBoard(int* board, int n) {
   for (int i = 0; i < n * n * n; i++) {
-    board[i] = 0;
+    *(board + i) = 0;
   }
 }
 
-int getNextHighestBlock(int* board, int n, int x, int y) {
-  int h = -1;
-  for (int i = 0; i < n; i++) {
+int getNextHighestBlock(int* board, int n, int x, int z) {
+  int hi = -1;
+
+  float h = n / 2.0f;
+  for (int i = -h; i < h; i++) {
     for (int j = 0; j < n; j++) {
-      for (int k = 0; k < n; k++) {
-        if (x == i && y == k && j > h) {
+      for (int k = -h; k < h; k++) {
+        if (x == i && z == k && j > hi) {
           if (board[i * n * n + j * n + k] != 0) {
-            h = j;
+            hi = j;
           }
         }
       }
     }
   }
-  return h + 1;
+  return hi + 1;
+}
+
+void placeBlock(int* board, int n, int x, int z, int p) {
+  float h = n / 2.0f;
+
+  int highest = getNextHighestBlock(board, n, x, z);
+
+  if (highest >= n) return;
+
+  *(board + x * n * n + highest * n + z) = p;
 }
 
 void renderBoard(int* board, int n, Vector2 sel) {
   //Render Grid
-  
+
   float h = n / 2.0f;
   float s = 5.0f;
 
@@ -38,7 +50,7 @@ void renderBoard(int* board, int n, Vector2 sel) {
     rlColor3f(0.75f, 0.75f, 0.75f);
     rlColor3f(0.75f, 0.75f, 0.75f);
     rlColor3f(0.75f, 0.75f, 0.75f);
-  
+
     rlVertex3f((float) i * s, 0.0f, (float) -h * s);
     rlVertex3f((float) i * s, 0.0f, (float) h * s);
     rlVertex3f((float) -h * s, 0.0f, (float) i * s);
@@ -47,22 +59,24 @@ void renderBoard(int* board, int n, Vector2 sel) {
 
   rlEnd();
 
-  
-  DrawCube({sel.x * s, 2.5f + getNextHighestBlock(board, n, sel.x, sel.y) * s, sel.y * s}, s, s, s, {255, 255, 255, 55});
-  DrawCubeWires({sel.x * s, 2.5f + getNextHighestBlock(board, n, sel.x, sel.y) * s, sel.y * s}, s, s, s, {255, 255, 255, 255});
+  //Render Cubes
 
-  //Draw Cubes
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-      for (int k = 0; k < n; k++) {
-        int v = board[i * n * n + j * n + k];
-        if (v != 0) {
-          DrawCube({(float) i * (n * n) * s, (float) j * n * s + 2.5f, (float) k * s}, s, s, s, (v == 1 ? Color{255, 0, 0, 55} : Color{0, 0, 255, 55}));
-          DrawCubeWires({(float) i * (n * n) * s, (float) j * n * s + 2.5f, (float) k * s}, s, s, s, (v == 1 ? Color{255, 0, 0, 255} : Color{0, 0, 255, 255}));
+  for (float i = -h + 0.5f; i < h; i++) {
+    for (float j = 0; j < n; j++) {
+      for (float k = -h + 0.5f; k < h; k++) {
+        int v = *(board + (int) i * n * n + (int) j * n + (int) k);
+        if (v == 1 || v == 2) {
+          Vector3 pos = {i * s, 2.5f + j * s, (float) k * s};
+          DrawCube(pos, s, s, s, (v == 1 ? Color{255, 0, 0, 45} : Color{0, 0, 255, 45}));
+          DrawCubeWires(pos, s, s, s, (v == 1 ? Color{255, 0, 0, 255} : Color{0, 0, 255, 255}));
         }
       }
     }
   }
+
+  Vector3 pos = {sel.x * s, 2.5f + getNextHighestBlock(board, n, sel.x, sel.y) * s, sel.y * s};
+  DrawCube(pos, s, s, s, {255, 255, 255, 55});
+  DrawCubeWires(pos, s, s, s, {255, 255, 255, 255});
 }
 
 int main() {
@@ -77,20 +91,20 @@ int main() {
   int* board = new int[n * n * n];
   resetBoard(board, n);
 
-  *(board) = 1;
-  *(board + 1) = 2;
-
   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "ConnectX 3D");
 
   SetTargetFPS(60);
 
+  float f = 0.2f;
+
   Camera3D* camera = new Camera3D();
-  camera->position = (Vector3) {30.0f, 20.0f, 30.0f};
+  camera->position = (Vector3) {30.0f * n * f, 20.0f * n * f * 1.5f, 30.0f * n * f};
   camera->target = (Vector3){0.0f, 0.0f, 0.0f};
   camera->up = (Vector3){0.0f, 1.0f, 0.0f};
   camera->fovy = 60.0f;
   camera->projection = CAMERA_PERSPECTIVE;
 
+  int p = 1;
   while(!WindowShouldClose()) {
     //Handle Input
     if (IsKeyPressed(KEY_W)) {
@@ -102,10 +116,11 @@ int main() {
     } else if (IsKeyPressed(KEY_D)) {
       sel.y--;
     } else if (IsKeyPressed(KEY_SPACE)) {
-      std::cout << getNextHighestBlock(board, n, sel.x, sel.y) << std::endl;
+      placeBlock(board, n, sel.x, sel.y, p);
+      p = 3 - p;
     }
 
-    //UpdateCamera(camera, CAMERA_ORBITAL);
+    //UpdateCamera(camera, CAMERA_PERSPECTIVE);
 
     BeginDrawing();
 
